b10167ead651d6bafef6cb04e347c15f7aa9b4bd
wlcharlie <wl00606352@gmail.com>
2021-05-14 16:50:33 +0800

    feat: add "delete function" /w method-override

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const hbsHelpers = require('handlebars-helpers')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
+
 const Record = require('./models/record')
 const categoryFilter = require('./models/category-filter')
 const modifyRecord = require('./models/modify-record')
@@ -26,6 +28,7 @@ app.engine('handlebars', exphbs({ helpers, defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   const category = req.query.category || ''
@@ -43,15 +46,17 @@ app.get('/records/new', (req, res) => {
 })
 
 app.post('/records', (req, res) => {
-
   let newRecord = req.body
   modifyRecord(newRecord)
   setTimeout(() => {
     res.redirect('/')
-  }, 1000);
-
+  }, 0000);
 })
 
+app.delete('/records/:id', (req, res) => {
+  Record.findByIdAndDelete(req.params.id)
+    .then(() => res.redirect(req.get('referer'))) //(分類頁面)哪裡刪除回到哪裡
+})
 
 app.listen(port, () => {
   console.log(`app is listening on http://localhost:${port}`)
