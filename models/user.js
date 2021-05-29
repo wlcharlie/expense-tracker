5ed5862623e3956c58029e90bbcb455478c447ab
wlcharlie <wl00606352@gmail.com>
2021-05-30 00:37:18 +0800

    feat: user schema

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+})
+
+module.exports = mongoose.model('User', userSchema)
