093f4fb09a1d6dcbf791bab8e12016ee89b15b44
wlcharlie <wl00606352@gmail.com>
2021-05-30 17:47:59 +0800

    minor changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const db = mongoose.connection
 
