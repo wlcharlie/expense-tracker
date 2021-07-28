a75e5ecdf468c65c9f08bcfc49e5a86db0641866
Charlie <wl00606352@gmail.com>
2021-07-28 23:19:04 +0800

    refactor user register

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,45 +1,40 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const passport = require('passport')
-const User = require('../../models/user')
-const bcrypt = require('bcryptjs')
+const passport = require("passport")
+const User = require("../../models/user")
+const bcrypt = require("bcryptjs")
 
-router.get('/', (req, res) => {
-  res.render('user')
+router.get("/", (req, res) => {
+  return res.render("user")
 })
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/user',
-  failureFlash: true
-}))
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/user",
+    failureFlash: true,
+  })
+)
 
-router.post('/register', (req, res) => {
+router.post("/register", async (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   if (password !== confirmPassword) {
-    req.flash('errorRegMsg', '密碼與確認密碼不同')
-    return res.render('user', {
+    req.flash("errorRegMsg", "密碼與確認密碼不同")
+    return res.render("user", {
       name,
       email,
     })
   }
 
-  bcrypt.genSalt(10)
-    .then(salt => bcrypt.hash(password, salt))
-    .then(hash => User.create({
-      name,
-      email,
-      password: hash
-    }))
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
+  const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+  await User.create({ name, email, password: hash })
+  return res.redirect("/")
 })
 
-
-
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout()
-  res.redirect('/user')
+  res.redirect("/user")
 })
 
 module.exports = router
